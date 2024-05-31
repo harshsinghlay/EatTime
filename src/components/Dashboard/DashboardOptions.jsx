fa0c7be2757324dc3580e2061c0e78703188609d
Harsh Singhlay <harshsinghlay9990@gmail.com>
2024-05-31 17:14:42 -0500

    first commit

--- src/components/Dashboard/DashboardOptions.jsx
+++ src/components/Dashboard/DashboardOptions.jsx
@@ -56,14 +56,14 @@ function DashboardOptions() {
 
       {/*========= Dashboard =========*/}
       <section >
-        <p className='text-sm lg:text-xs xl:text-sm  text-gray-500'>DASHBOARD</p>
+        <p className='text-sm   text-gray-500'>DASHBOARD</p>
         <div className='flex flex-col gap-4 py-4'>
           {dashboardLinks.map((link, index) => (
             <NavLink to={link.url}
               key={index}
               className={({ isActive }) => `${isActive ? "text-red-500" : "text-gray-600"} flex justify-between gap-4 items-center text-sm sm:text-base lg:text-sm `}
             >
-              <div className='flex gap-2 items-center text-gray-600'>
+              <div className='flex gap-2 items-center '>
                 <span className='text-xl lg:text-lg'>{link.icon}</span>
                 <span className='text-lg lg:text-base'>{link.linkname}</span>
               </div>
@@ -75,7 +75,7 @@ function DashboardOptions() {
 
       {/*========= Account Settings =========*/}
       <section >
-        <p className='text-sm lg:text-xs xl:text-sm text-gray-500'>ACCOUNT SETTINGS</p>
+        <p className='text-sm  text-gray-500'>ACCOUNT SETTINGS</p>
         <div className='flex flex-col gap-4 py-4'>
           {accountLinks.map((link, index) => (
             <NavLink to={link.url}
