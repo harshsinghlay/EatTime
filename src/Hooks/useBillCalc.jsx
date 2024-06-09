ef8cc6838ae80428cfbbdc0535b43173feeb2bda
Harsh Singhlay <harshsinghlay9990@gmail.com>
2024-06-09 13:35:58 -0500

    first commit

--- src/Hooks/useBillCalc.jsx
+++ src/Hooks/useBillCalc.jsx
@@ -12,16 +12,14 @@ function useBillCalc() {
         const discount = (total * 10) / 100;
         const discountedPrice = total - discount;
         const GST = (discountedPrice * 15) / 100;
-        const deliveryCharges = 100 > total ? 40 : 60;
+        const deliveryCharges = totalItems > 0 ? (100 > total ? 40 : 60) : 0;
         const subTotal = discountedPrice + deliveryCharges + GST;
         return { deliveryCharges, subTotal, total, totalItems, GST, discount, discountedPrice };
     };
 
     useEffect(() => {
-        if (cart.length > 0) {
-            setCalculatedValues(calculateValues());
-        }
-    }, [cart.length]);
+        setCalculatedValues(calculateValues());
+    }, [cart]);
     return calculatedValues
 }
 
